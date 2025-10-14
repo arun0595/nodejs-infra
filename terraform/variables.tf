bf90e569415c5466c5ea3e5cf01f18800c066f29
arun0595 <117884579+arun0595@users.noreply.github.com>
2025-10-14 09:41:06 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "region" {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+}
+
+variable "app_name" {
+  description = "The name of the application"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC where resources will be created"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs for the ECS service"
+  type        = list(string)
+}
+
+variable "security_groups" {
+  description = "A list of security group IDs for the ECS service and load balancer"
+  type        = list(string)
+}
