7e5aace58b32ab5363cfd745aee5b69d6558f4f9
arun0595 <117884579+arun0595@users.noreply.github.com>
2025-10-14 11:05:51 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
+    bucket = "arun-terraform-sta"
     key    = "ecs/hello-world-app/terraform.tfstate"
     region = "ap-south-1"
   }
